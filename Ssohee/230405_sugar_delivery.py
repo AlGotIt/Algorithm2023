d3efd7567d91031354483dd1ed79e1a6fee09dd2
soaringsso <ksi06191@naver.com>
2023-04-10 15:37:02 +0900

    230405_sugar_delivery.py

--- Ssohee/230405_sugar_delivery.py
+++ Ssohee/230405_sugar_delivery.py
@@ -1,4 +1,5 @@
 # 230405 백준 2839 설탕배달
+# https://www.acmicpc.net/problem/2839
 
 n = int(input())
 cnt = 0
@@ -10,33 +11,15 @@ if n == 4:
 else: # n이 6이상
     rest = n % 5
     cnt = n // 5
-    if rest == 0:   # 5로 나누어진다면
-        print(cnt)
-    else:   # 5로 안나누어진다면
-        while cnt >= 0:
-            if rest % 3 == 0:   #3으로 나누어지면
-                cnt += rest // 3
+    if rest != 0:   # 5로 안나누어지면
+        while cnt >= 0: 
+            if rest % 3 == 0:   # 3으로 나누어지면
+                cnt += rest // 3    # 3의 몫만큼 cnt추가
                 break
             else:   # 3으로 안 나뉘면
                 rest += 5 
                 cnt -= 1
-        cnt = -1
-
-print(cnt)          
-
-
-
-# num = int(input())
-# count = 0
-
-# while num >= 0:
-#   if num % 5 == 0:
-#     count += int(num // 5)
-#     print(count)
-#     break
-  
-#   num -= 3
-#   count += 1
-  
-# else:
-#   print(-1)
+if cnt > 0 :
+    print(cnt)
+else:
+    print(-1)          
